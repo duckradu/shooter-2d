df5f6b466d726475f39e5a17279a8f458fb87366
duckradu <radu.dascalu@pm.me>
2024-07-20 19:11:56 +0100

    Add Weapon & CursorPosition components

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::f32::consts::PI;
+
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
-use bevy::math::vec3;
+use bevy::math::{vec2, vec3};
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 const WINDOW_WIDTH: f32 = 1200.0;
 const WINDOW_HEIGHT: f32 = 700.0;
@@ -22,12 +25,19 @@ const PLAYER_SPEED: f32 = 2.0;
 // Resources
 #[derive(Resource)]
 struct GlobalTextureAtlasHandle(Option<Handle<TextureAtlasLayout>>);
+
 #[derive(Resource)]
 struct GlobalSpriteSheetHandle(Option<Handle<Image>>);
+
+#[derive(Resource)]
+struct CursorPosition(Option<Vec2>);
 
 // Components
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct Weapon;
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
 enum GameState {
@@ -61,6 +71,7 @@ fn main() {
         // Custom resources
         .insert_resource(GlobalTextureAtlasHandle(None))
         .insert_resource(GlobalSpriteSheetHandle(None))
+        .insert_resource(CursorPosition(None))
         // Plugins
         .add_plugins(LogDiagnosticsPlugin::default())
         .add_plugins(FrameTimeDiagnosticsPlugin)
@@ -69,8 +80,15 @@ fn main() {
         // Systems
         .add_systems(OnEnter(GameState::Loading), load_assets)
         .add_systems(OnEnter(GameState::Bootstraping), (setup_camera, init_world))
-        .add_systems(Update, (handle_input).run_if(in_state(GameState::Playing)))
-        // .add_systems(Startup, (setup_camera, spawn_player))
+        .add_systems(
+            Update,
+            (
+                update_cursor_position,
+                handle_input,
+                update_weapon_transform,
+            )
+                .run_if(in_state(GameState::Playing)),
+        )
         .run();
 }
 
@@ -118,8 +136,38 @@ fn init_world(
         },
         Player,
     ));
+    commands.spawn((
+        SpriteBundle {
+            texture: image_handle.0.clone().unwrap(),
+            transform: Transform::from_scale(Vec3::splat(SPRITE_SCALE_FACTOR)),
+            ..default()
+        },
+        TextureAtlas {
+            layout: texture_atlas.0.clone().unwrap(),
+            index: 17,
+        },
+        Weapon,
+    ));
 
     next_state.set(GameState::Playing);
+}
+
+fn update_cursor_position(
+    mut cursor_position: ResMut<CursorPosition>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    camera_query: Query<(&Camera, &GlobalTransform), With<Camera>>,
+) {
+    if window_query.is_empty() || camera_query.is_empty() {
+        cursor_position.0 = None;
+    }
+
+    let (camera, camera_transform) = camera_query.single();
+    let window = window_query.single();
+
+    cursor_position.0 = window
+        .cursor_position()
+        .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
+        .map(|ray| ray.origin.truncate())
 }
 
 fn handle_input(
@@ -161,4 +209,38 @@ fn handle_input(
     if delta.is_finite() && (up_key || right_key || down_key || left_key) {
         transform.translation += vec3(delta.x, delta.y, 0.0) * PLAYER_SPEED;
     }
+}
+
+fn update_weapon_transform(
+    player_query: Query<&Transform, With<Player>>,
+    mut weapon_query: Query<&mut Transform, (With<Weapon>, Without<Player>)>,
+    cursor_position: Res<CursorPosition>,
+) {
+    if player_query.is_empty() || weapon_query.is_empty() {
+        return;
+    }
+
+    let player_position = player_query.single().translation.truncate();
+    let cursor_position = match cursor_position.0 {
+        Some(pos) => pos,
+        None => player_position,
+    };
+    let mut weapon_transform = weapon_query.single_mut();
+
+    let angle =
+        (player_position.y - cursor_position.y).atan2(player_position.x - cursor_position.x) + PI;
+
+    weapon_transform.rotation = Quat::from_rotation_z(angle);
+
+    let offset = 20.0;
+    let new_weapon_position = vec2(
+        player_position.x + offset * angle.cos() - 5.0,
+        player_position.y + offset * angle.sin() - 15.0,
+    );
+
+    weapon_transform.translation = vec3(
+        new_weapon_position.x,
+        new_weapon_position.y,
+        weapon_transform.translation.z,
+    );
 }
